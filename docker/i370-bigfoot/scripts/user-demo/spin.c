c640f870a8ac2165cb8cbfe1db63a49c13efee2f
Linas Vepštas <linasvepstas@gmail.com>
2024-09-25 11:57:25 -0500

    Work on userspace demo

---
+++ docker/i370-bigfoot/scripts/user-demo/spin.c
@@ -0,0 +1,15 @@
+
+int main(int argc, char* argv)
+{
+	int i;
+	int x = argc;
+	int y = argv[1];
+	int buf[1000];
+	for (i=0; i<1000; i++) buf[i] = i;
+
+	while (1) {
+		for (i=0; i<1000; i++)
+			buf[i] += buf [(i+1)%1000];
+	}
+	return 3;
+}
