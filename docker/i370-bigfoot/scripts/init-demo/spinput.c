087abf319f37d7dd5ce96f394a4995bd92813c89
Linas Vepštas <linasvepstas@gmail.com>
2024-09-27 16:42:42 -0500

    Work on an input demo

--- docker/i370-bigfoot/scripts/init-demo/spinput.c
+++ docker/i370-bigfoot/scripts/init-demo/spinput.c
@@ -1,5 +1,5 @@
 /*
- * Simple i370 Linux Userspace Demo
+ * Simple i370 Linux Userspace Console I/O Demo
  *
  * After the Linux kernel boots, the first program it runs is /sbin/init
  * or any other program specifiec by the `init=` argument on the boot
@@ -9,32 +9,43 @@
  * any C library, and so none of the usual C library routines are
  * available. However, system calls can be made, by including the
  * `demo-unistd.h` include file. It defines just enough for a handful
- * of system calls, including the write() subroutine. This demo
- * makes use of that, to spin in a loop for a while, and then print
- * to the console. This only does output; see the `spinput.c` demo
- * for a version of this that also reads.
+ * of system calls, including the read() and write() subroutines. This
+ * demo makes use of that, to spin in a loop for a while, attempt to
+ * read from the tty, and then echo that back out.
+ *
+ * See the `spin.c` demo for a simpler version, that only writes.
  */
 #include "demo-unistd.h"
 
 int main(int argc, char* argv)
 {
+	int rc;
 	int i, j;
-	int c = argc;
-	// char * x = argv[0]; Not today!
+
+	/* Keyboard input data */
+#define BUFSZ 120
+	char inbuf[BUFSZ];
 
 	/* Don't forget to mknod and create this device! */
-	int ttyfd = open("/dev/console", O_WRONLY, 0);
+	int ttyfd = open("/dev/tty0", O_RDWR, 0);
 	write(ttyfd, "Hello\n", 7);
 
-	int buf[1000];
-	for (i=0; i<1000; i++) buf[i] = i;
+	int data[1000];
+	for (i=0; i<1000; i++) data[i] = i;
 
 	while (1) {
+		/* Waste some CPU cycles */
 		for (j=0; j<1000; j++) {
 			for (i=0; i<1000; i++)
-				buf[i] += buf [(i+1)%1000];
+				data[i] += data [(i+1)%1000];
 		}
-		write(ttyfd, "whirl\n", 7);
+
+		rc = read(ttyfd, inbuf, BUFSZ);
+		if (rc < 0)
+			write(ttyfd, "input error\n", 13);
+		write(ttyfd, "OK\n", 4);
+
+		write(ttyfd, "whorl\n", 7);
 	}
 	return 3;
 }
